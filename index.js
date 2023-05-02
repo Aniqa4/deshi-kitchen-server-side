b2ee355c9e98eddc42cb37ba2a33c953dec8403f
Aniqa Chowdhury <aanika.chowdhury04@gmail.com>
2023-05-03 00:19:36 +0600

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const  app = express();
+const cors= require('cors');
 port =process.env.PORT || 5000;
 
 const chefs = require('./data/chefs.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('dragon is running')
