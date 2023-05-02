8a7f3ea83dfd5313e4715898e98b61ee7746c94b
Aniqa Chowdhury <aanika.chowdhury04@gmail.com>
2023-05-02 19:34:59 +0600

    added json file

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const  app = express();
 port =process.env.PORT || 5000;
 
+const chefs = require('./data/chefs.json');
+
 app.get('/', (req, res) => {
     res.send('dragon is running')
+})
+
+app.get('/chefs', (req, res) =>{
+    res.send(chefs);
 })
 
 app.listen(port, (req,res) =>{
